23a925984c56719ff03de7345f5799f17104fe8a
Nikhilbabuthachattil <thachattilnikhil@gmail.com>
2023-09-27 11:50:42 +0530

    seventh commit

--- src/components/menuBar.js
+++ src/components/menuBar.js
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import { useState } from "react";
-
+import img20 from "../assets/menubar.png";
 import { NavLink } from "react-router-dom";
 
 function MenuBar() {
@@ -74,22 +74,13 @@ function MenuBar() {
           id="menu-btn"
           className="z-30 static right-0 text-white px-5  lg:hidden py-5"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={3}
-            stroke="currentColor"
-            className="w-6 h-6 cursor-pointer text-dred p-1 bg-dcream rounded-full "
+          <img
             onClick={() => setOpen(!open)}
             ref={svgRef}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
+            className="w-6 h-6 cursor-pointer "
+            src={img20}
+            alt=""
+          />
         </div>
       </nav>
     </div>
